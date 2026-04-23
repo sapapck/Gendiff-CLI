beda5400e97cfa092da2ab3738ffcc792edcca83
Voti <isma.boss93@mail.ru>
2026-04-23 19:53:16 +0300

    fixed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-import { program } from "commander";
-import { readFileSync } from "fs";
-import genDiff from "../src/index.js";
+import { readFileSync } from 'fs';
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
-const packageJsonPath = new URL("../package.json", import.meta.url);
+const packageJsonPath = new URL('../package.json', import.meta.url);
 const { version } = JSON.parse(readFileSync(packageJsonPath));
 
 program
-  .name("gendiff")
-  .description("Compares two configuration files and shows a difference.")
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
   .version(version)
-  .option("-f, --format <type>", "output format", "stylish")
-  .arguments("<filepath1> <filepath2>")
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
